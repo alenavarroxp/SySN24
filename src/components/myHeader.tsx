3859768fe03d4da6047139ae5a409073779d0b76
alenavarroxp <alejandropvp13@gmail.com>
2024-12-27 17:43:38 +0100

    InsertProducts Lambda added in SAM and frontend with a button

--- src/components/myHeader.tsx
+++ src/components/myHeader.tsx
@@ -1,21 +1,50 @@
 import React from 'react';
+import { FaDatabase } from 'react-icons/fa';
+import { insertProduct } from '../utils/api/products';
 
 interface MyHeaderProps {
     title: string;
 }
 
-const MyHeader: React.FC<MyHeaderProps> = ({ title }) => (
-    <header className="p-8 flex justify-between items-center text-white border-b-2 shadow-2xl">
-        <div className='w-48'>
-            <p className='text-xs mb-1'>Alejandro Navarro de la Cruz</p>
-            <p className='text-xs mb-1'>Alonso Illán Martínez del Santo</p>
-        </div>
-        <h1 className="text-5xl font-bold drop-shadow-lg text-gray-100">{title}</h1>
-        <div className='w-48 text-end'>
-            <p className='text-xs mb-1'>Javier Domingo Collado</p>
-            <p className='text-xs mb-1'>Juan Miguel García González</p>
-        </div>
-    </header>
-);
+const MyHeader: React.FC<MyHeaderProps> = ({ title }) => {
+    // Puedes agregar aquí cualquier lógica o constantes que necesites
+    const handleButtonClick = async () => {
+        await insertProduct();
+        setTimeout(async() => {
+            window.location.reload();
+        }, 1000);
+    };
+
+    const authorNames = [
+        "Alejandro Navarro de la Cruz",
+        "Alonso Illán Martínez del Santo",
+        "Javier Domingo Collado",
+        "Juan Miguel García González"
+    ];
+
+    return (
+        <header className="p-8 flex justify-between items-center text-white border-b-2 shadow-2xl">
+            <div className='w-48'>
+                {authorNames.slice(0, 2).map((name, index) => (
+                    <p key={index} className='text-xs mb-1'>{name}</p>
+                ))}
+            </div>
+            <div className="text-5xl font-bold drop-shadow-lg text-gray-100 flex items-center justify-center">
+                {title}
+                <button 
+                    className="ml-4 p-2 bg-green-500 text-white rounded-full hover:bg-green-600" 
+                    onClick={handleButtonClick}
+                >
+                    <FaDatabase className="h-6 w-6" />
+                </button>
+            </div>
+            <div className='w-48 text-end'>
+                {authorNames.slice(2).map((name, index) => (
+                    <p key={index} className='text-xs mb-1'>{name}</p>
+                ))}
+            </div>
+        </header>
+    );
+};
 
 export default MyHeader;
--- src/utils/api/products.ts
+++ src/utils/api/products.ts
@@ -16,3 +16,22 @@ export const fetchProducts = async (): Promise<Product[]> => {
         return [];
     }
 };
+
+export const insertProduct = async (): Promise<Product> => {
+    try {
+        const response = await fetch('https://b7yrvbrh47.execute-api.us-east-1.amazonaws.com/Prod/products', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (!response.ok) {
+            throw new Error('Error inserting product');
+        }
+        const product = await response.json();
+        return product;
+    } catch (error) {
+        console.error("Error inserting product:", error);
+        throw error;
+    }
+};
